530c552faacff770b8acbef61df79ba90e495967
Arm1stice <arm1stice@arm1stice.com>
2018-07-17 02:14:03 -0600

    Port setInterval to Go

---
+++ setInterval.go
@@ -0,0 +1,43 @@
+package main
+
+import "time"
+
+// Code taken from https://www.loxodrome.io/post/set-timeout-interval-go/
+func setInterval(someFunc func(), milliseconds int, async bool) chan bool {
+
+	// How often to fire the passed in function
+	// in milliseconds
+	interval := time.Duration(milliseconds) * time.Millisecond
+
+	// Setup the ticket and the channel to signal
+	// the ending of the interval
+	ticker := time.NewTicker(interval)
+	clear := make(chan bool)
+
+	// Put the selection in a go routine
+	// so that the for loop is none blocking
+	go func() {
+		for {
+
+			select {
+			case <-ticker.C:
+				if async {
+					// This won't block
+					go someFunc()
+				} else {
+					// This will block
+					someFunc()
+				}
+			case <-clear:
+				ticker.Stop()
+				return
+			}
+
+		}
+	}()
+
+	// We return the channel so we can pass in
+	// a value to it to clear the interval
+	return clear
+
+}
